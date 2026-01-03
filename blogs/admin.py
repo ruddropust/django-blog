41b4e9b7d34a94a94dc7e0997b56e54b267e02ae
ruddropust <sharkabir01@gmail.com>
2026-01-03 12:36:14 +0600

    blog model work

---
+++ blogs/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Blog
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','category_name', 'created_at', 'updated_at']
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ['id','title', 'category', 'author', 'status', 'is_featured']
+    search_fields = ['id','title', 'category__category_name', 'author__username',  'status']
+    list_editable=['status', 'is_featured']
+
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blog, BlogAdmin)
